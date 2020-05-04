6a18df3e3054769a3df50392b70d48bc627471bf
Jisan Haider Joy <jisanhaider76@gmail.com>
2020-05-05 02:00:04 +0600

    Terminal Shortcut Tool added

---
+++ Assets/Tools/OpenTerminalShortcut/Editor/OpenTerminalAtProject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public static class OpenTerminalAtProject
+{
+    [MenuItem("Tools/Headshot Games/Open Terminal at Project %#.")]
+    public static void OpenTerminal()
+    {
+        string projectPath = Application.dataPath;
+        projectPath = projectPath.Substring(0, projectPath.Length - 6);
+        Process.Start("open", "-a Terminal " + projectPath + " -n");
+    }
+}
